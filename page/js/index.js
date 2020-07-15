532dea3a95936d8eb531a955e045fef097b4c5a5
Gip886 <49873054+Gip886@users.noreply.github.com>
2020-07-15 19:07:20 +0800

    Add files via upload

---
+++ page/js/index.js
@@ -0,0 +1,115 @@
+$(function() {
+	
+	//	点击注册按钮
+	$("#regist").click(function() {
+		$("#register").animate({
+			// display:'block',
+			height: 'show',
+			opacity: '80%'
+		}, "slow");
+		//加点高斯模糊效果
+		$(".login").addClass("blur");
+
+	});
+
+	//点击关闭按钮
+	$("#closer").click(function() {
+		$("#register").slideToggle("slow");
+		$(".login").removeClass("blur");
+	});
+	//点击提交按钮
+	var data = [{
+		username: "yang",
+		password: "123"
+	}];
+	
+	$("#register-sign").click(function() {
+		var name = $("#register-username").val();
+		var p1 = $("#register-p1").val();
+		var p2 = $("#register-p2").val();
+		console.log(p1);
+		console.log(p2);
+		if(window.sessionStorage.my){
+		   console.log("266");
+		   var nbb=JSON.parse( sessionStorage.my);
+		   console.log(nbb);
+		   data=nbb;
+			
+		}
+		if(name ==""||p1 ==""||p2==""){
+			alert("填写不能为空");
+			document.getElementById("registform").reset();
+			return;
+		}
+		for (var i = 0; i < data.length; i++) {
+			if(data[i].username == name){
+				alert("用户名已存在,请重新输入!");
+				document.getElementById("registform").reset();
+				return;
+			}
+			
+		}
+		if (p1 != p2) {
+			alert("两次密码不一致,重新输入!");
+			document.getElementById("registform").reset();
+		} else {
+			data.push({
+				username: name,
+				password: p1
+			});
+			console.log(data);
+			alert("注册成功!");
+			$("#register").slideToggle("slow");
+			$(".login").removeClass("blur");
+			document.getElementById("registform").reset();
+			sessionStorage.my=JSON.stringify(data);
+		}
+	});
+
+	//点击登录按钮
+	$("#login-button").click(function(){
+		var name = $("#login-name").val();
+		var password = $("#login-password").val();
+		var flag=false;
+		
+		if(window.sessionStorage.my){
+		   console.log("266");
+		   var nbb=JSON.parse( sessionStorage.my);
+		   console.log(nbb);
+		   data=nbb;
+			
+		}
+		
+		
+		for (var i = 0; i < data.length; i++) {
+			// console.log(typeof(data[i].username));
+			// console.log(typeof(data[i].password));
+			// console.log(typeof(name));
+			// console.log(typeof(password));
+			if (data[i].username == name && data[i].password == password){
+				alert("验证成功!");
+				flag=true;
+				window.location.href="page/html/main.html";
+				break;
+			}
+		}
+		if(!flag){
+			alert("登录失败,请检查用户名和密码是否正确");
+		}
+		
+	});
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ page/js/main.js
@@ -0,0 +1,39 @@
+$(function() {
+	var data = [0, 200, 250, 630, 77, 55, 0, 340, 8];
+
+	for (var i = 1; i <= data.length; i++) {
+		var ready1 = "#num" + i;
+		var ready2 = "#bar" + i
+		$(ready1).text(data[i]);
+		$(ready2).text(data[i]);
+		count();
+	}
+
+
+	$(".col-sm-3 .btn").click(function(event) {
+		console.log(event.target.id);
+		alert("非常感谢您的投票!");
+		var num = event.target.id.replace("b", "");
+		console.log(num);
+		var a = "#num" + num;
+		data[num]++;
+		$(a).text(data[num]);
+		var b = "#bar" + num;
+		$(b).text(data[num]);
+		count();
+	});
+
+
+	//计算投票比例
+	function count() {
+		for (var i = 1; i <= data.length; i++) {
+			var perent = parseFloat(data[i] * 0.1);
+			var ready2 = "#bar" + i
+			console.log(perent + '%');
+			$(ready2).css("width", perent + '%');
+		}
+	}
+
+
+
+});
